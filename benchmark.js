dad9402315a12bfc937a169b054774b117d2078c
vec <vec@torzo.club>
2018-08-14 01:09:41 +0800

    update bechmark and test

--- benchmark.js
+++ benchmark.js
@@ -1,10 +1,10 @@
 const chalk = require('chalk')
 
 const Benchmark = require('benchmark')
-var suite = new Benchmark.Suite
+const suite = new Benchmark.Suite
+const fast_suite = new Benchmark.Suite
 
-const fast_check_luhn = require('./')
-const check_luhn = fast_check_luhn.factory()
+const check_luhn = require('./')
 check_luhn.fast_toNumberArray.clear()
 
 const correct_card = '6225365271562822'
@@ -18,18 +18,29 @@ suite
 .add(`check-luhn#${chalk.yellow('IncorrectCard')}`, () => {
   check_luhn(incorrect_card)
 })
+// add listeners
+.on('cycle', function(event) {
+  console.log(String(event.target))
+})
+.on('complete', function() {
+  console.log('\nFastest is ' + this.filter('fastest').map('name') + '\n')
+})
+.run()
+// .run({ 'async': true })
+
+check_luhn.fast_toNumberArray.init([15, 16, 17, 18, 19, 20])
+
+fast_suite
 .add(`check-luhn#${chalk.cyan('CorrectCard')} with ${chalk.green('fast_toNumberArray')}`, () => {
-  fast_check_luhn(correct_card)
+  check_luhn(correct_card)
 })
 .add(`check-luhn#${chalk.yellow('IncorrectCard')} with ${chalk.green('fast_toNumberArray')}`, () => {
-  fast_check_luhn(incorrect_card)
+  check_luhn(incorrect_card)
 })
-// add listeners
 .on('cycle', function(event) {
   console.log(String(event.target))
 })
 .on('complete', function() {
   console.log('\nFastest is ' + this.filter('fastest').map('name'))
 })
-// run async
-.run({ 'async': true })
+.run()
--- test/index.js
+++ test/index.js
@@ -1,22 +1,6 @@
 import test from 'ava'
 import check from '../index'
 
-test('由 factory 方法出来的函数们并不相等', t => {
-  t.false(check.factory() === check.factory())
-})
-
-test('包括 factory 方法出来的 check 函数都不共用同个 fast_toNumberArray', t => {
-  const factoryCheck = check.factory()
-  factoryCheck.fast_toNumberArray.clear()
-
-  const set_arr = [30, 31, 32]
-  factoryCheck.fast_toNumberArray.init(set_arr)
-
-  set_arr.forEach(set_item => {
-    t.false( check.fast_toNumberArray.hasOwnProperty(set_item) )
-  })
-})
-
 test('输入必须是字符串类型', t => {
   [6225365271562822, 1.1, null, undefined, {}, [], NaN, Infinity].forEach(value => {
     t.throws(() => check(value), TypeError)
@@ -28,18 +12,22 @@ test('输入必须是字符串类型', t => {
   })
 })
 
-function incorrectTest(prefix) {
-  test(`${prefix} 正确的卡号 '6225365271562822'`, t => {
-    t.true(check('6225365271562822'))
-  })
+test(`正确的卡号 '6225365271562822'`, t => {
+  check.fast_toNumberArray.clear()
+  t.true(check('6225365271562822'))
+})
 
-  test(`${prefix} 错误的卡号 '6225365271562823'`, t => {
-    t.false(check('6225365271562823'))
-  })
-}
+test(`错误的卡号 '6225365271562823'`, t => {
+  check.fast_toNumberArray.clear()
+  t.false(check('6225365271562823'))
+})
 
-incorrectTest('normalToNumberArray ', check.factory())
+test(`fast_toNumberArray 正确的卡号 '6225365271562822'`, t => {
+  check.fast_toNumberArray.init([15, 16, 17, 18, 19, 20])
+  t.true(check('6225365271562822'))
+})
 
-const fast_check = check.factory()
-fast_check.fast_toNumberArray.init([15, 16, 17, 18, 19])
-incorrectTest('fastToNumberArray ', fast_check)
+test(`fast_toNumberArray 错误的卡号 '6225365271562823'`, t => {
+  check.fast_toNumberArray.init([15, 16, 17, 18, 19, 20])
+  t.false(check('6225365271562823'))
+})
