f475a493afdd2f72431111d30c9fe510f545727d
vec <vec@torzo.club>
2018-08-14 00:51:17 +0800

    add fast_toNumberArray item

--- benchmark.js
+++ benchmark.js
@@ -1,23 +1,35 @@
+const chalk = require('chalk')
+
 const Benchmark = require('benchmark')
 var suite = new Benchmark.Suite
 
-const check_luhn = require('./')
+const fast_check_luhn = require('./')
+const check_luhn = fast_check_luhn.factory()
+check_luhn.fast_toNumberArray.clear()
+
 const correct_card = '6225365271562822'
 const incorrect_card = '6225365271562823'
 
 // add tests
-suite.add('check-luhn#CorrectCard', function() {
+suite
+.add(`check-luhn#${chalk.cyan('CorrectCard')}`, () => {
   check_luhn(correct_card)
 })
-.add('check-luhn#IncorrectCard', function () {
+.add(`check-luhn#${chalk.yellow('IncorrectCard')}`, () => {
   check_luhn(incorrect_card)
+})
+.add(`check-luhn#${chalk.cyan('CorrectCard')} with ${chalk.green('fast_toNumberArray')}`, () => {
+  fast_check_luhn(correct_card)
+})
+.add(`check-luhn#${chalk.yellow('IncorrectCard')} with ${chalk.green('fast_toNumberArray')}`, () => {
+  fast_check_luhn(incorrect_card)
 })
 // add listeners
 .on('cycle', function(event) {
   console.log(String(event.target))
 })
 .on('complete', function() {
-  console.log('Fastest is ' + this.filter('fastest').map('name'))
+  console.log('\nFastest is ' + this.filter('fastest').map('name'))
 })
 // run async
 .run({ 'async': true })
